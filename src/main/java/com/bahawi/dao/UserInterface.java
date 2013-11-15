70855effd6a57ba0137b63e5dc0e419acbc1fe15
bahawi <ismailaitbaha89@gmail.com>
2013-11-15 10:01:45 +0000

    vider le master

--- src/main/java/com/bahawi/dao/UserInterface.java
+++
@@ -1,9 +0,0 @@
-package com.bahawi.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bahawi.entities.Person;
-
-public interface UserInterface extends JpaRepository<Person, Long> {
-
-}
